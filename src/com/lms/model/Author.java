cfecdf8af492c767e767fcd3a89c4795ea93b3d8
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-03-30 16:31:23 -0400

    models added

--- src/com/lms/model/Author.java
+++ src/com/lms/model/Author.java
@@ -1,10 +1,7 @@
 package com.lms.model;
 
-<<<<<<< HEAD
 import java.util.Objects;
 
-=======
->>>>>>> models added
 public final class Author {
 	private final int id;
 	private String name;
@@ -28,7 +25,6 @@ public final class Author {
 
 	@Override
 	public int hashCode() {
-<<<<<<< HEAD
 		return id;
 	}
 
@@ -42,32 +38,6 @@ public final class Author {
 		} else {
 			return false;
 		}
-=======
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Author other = (Author) obj;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
->>>>>>> models added
 	}
 
 	@Override
--- src/com/lms/model/Publisher.java
+++ src/com/lms/model/Publisher.java
@@ -1,10 +1,7 @@
 package com.lms.model;
 
-<<<<<<< HEAD
 import java.util.Objects;
 
-=======
->>>>>>> models added
 public final class Publisher {
 	private final int id;
 	private String name;
@@ -53,7 +50,6 @@ public final class Publisher {
 
 	@Override
 	public int hashCode() {
-<<<<<<< HEAD
 		return id;
 	}
 
@@ -69,44 +65,6 @@ public final class Publisher {
 		} else {
 			return false;
 		}
-=======
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((address == null) ? 0 : address.hashCode());
-		result = prime * result + id;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Publisher other = (Publisher) obj;
-		if (address == null) {
-			if (other.address != null)
-				return false;
-		} else if (!address.equals(other.address))
-			return false;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (phone == null) {
-			if (other.phone != null)
-				return false;
-		} else if (!phone.equals(other.phone))
-			return false;
-		return true;
->>>>>>> models added
 	}
 
 	@Override
