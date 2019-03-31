d718440bec9531be00804c566d319f418c0da928
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-03-31 15:48:02 -0400

    removed unnecessary

--- src/com/lms/service/BorrowerService.java
+++ src/com/lms/service/BorrowerService.java
@@ -14,7 +14,7 @@ public interface BorrowerService extends Service {
 	public abstract Loan borrowBook(Borrower borrower, Book book , Branch branch, LocalDateTime dateOut, LocalDate dueDate); // create loan entry
 	public abstract Map<Book, Integer> getAllBranchCopies(Branch branch);
 
-	public abstract boolean returnBook(Borrower borrower, Book book , Branch branch, LocalDate dueDate); // this will be a custom method that deletes the Loan entry if the book is not overdue. Else does not delete the record and returns a false.
+	public abstract boolean returnBook(Borrower borrower, Book book , Branch branch); // this will be a custom method that deletes the Loan entry if the book is not overdue. Else does not delete the record and returns a false.
 	public abstract List<Branch> getAllBranchesWithLoan(); // this will be a custom method that gets all branches that the borrower borrowed a book from.
 	public abstract List<Book> getAllBorrowedBooks(); // this will be a custom method that gets all books that the borrower borrowed.
 
