155365bfe1641a2232ffbda58c20e42e0f86fb41
Jonathan Lovelace <kingjon3377@gmail.com>
2019-03-31 10:14:47 -0400

    BookLoansDao: Fix method signatures to refer to Loan identity.
    get() and getAll() need to return a Loan and a List of Loans,
    respectively, and update() and delete() need to take Loan
    parameters, not Books.

--- src/com/lms/dao/BookLoansDao.java
+++ src/com/lms/dao/BookLoansDao.java
@@ -12,8 +12,8 @@ import com.lms.model.Loan;
 
 public interface BookLoansDao {
 	public abstract Loan create(Book book, Borrower borrower, Branch branch, LocalDateTime dateOut, LocalDate dueDate) throws SQLException;
-	public abstract void update(Book book) throws SQLException;
-	public abstract void delete(Book book) throws SQLException;
-	public abstract Book get(Book book, Borrower borrower, Branch branch) throws SQLException;
-	public abstract List<Book> getAll() throws SQLException;
+	public abstract void update(Loan loan) throws SQLException;
+	public abstract void delete(Loan loan) throws SQLException;
+	public abstract Loan get(Book book, Borrower borrower, Branch branch) throws SQLException;
+	public abstract List<Loan> getAll() throws SQLException;
 }
