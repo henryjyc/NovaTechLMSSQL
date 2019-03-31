fd2ef13cab50de9f4898da19822625bcbbb121e5
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-03-31 09:27:13 -0400

    Book Loans does not extend Dao

--- src/com/lms/dao/BookLoans.java
+++ src/com/lms/dao/BookLoans.java
@@ -3,12 +3,17 @@ package com.lms.dao;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.lms.model.Book;
 import com.lms.model.Borrower;
 import com.lms.model.Branch;
 import com.lms.model.Loan;
 
-public interface BookLoans extends Dao<Loan> {
+public interface BookLoans {
 	public abstract Loan create(Book book, Borrower borrower, Branch branch, LocalDateTime dateOut, LocalDate dueDate) throws SQLException;
+	public abstract void update(Book t) throws SQLException;
+	public abstract void delete(Book t) throws SQLException;
+	public abstract Book get(Book book, Borrower borrower, Branch branch) throws SQLException;
+	public abstract List<Book> getAll() throws SQLException;
 }
