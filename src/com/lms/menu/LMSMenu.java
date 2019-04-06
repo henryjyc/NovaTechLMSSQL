b454418ae1bd48c31e1813cab4c38a8b05e8406c
Jonathan Lovelace <kingjon3377@gmail.com>
2019-04-06 09:27:24 -0400

    LMSMenu: Use no-arg service class constructors.

--- src/com/lms/menu/LMSMenu.java
+++ src/com/lms/menu/LMSMenu.java
@@ -1,22 +1,17 @@
 package com.lms.menu;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.Writer;
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.time.Clock;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.lms.dao.AuthorDaoImpl;
-import com.lms.dao.BookDaoImpl;
-import com.lms.dao.BookLoansDaoImpl;
-import com.lms.dao.BorrowerDaoImpl;
-import com.lms.dao.CopiesDaoImpl;
-import com.lms.dao.LibraryBranchDaoImpl;
-import com.lms.dao.PublisherDaoImpl;
 import com.lms.service.AdministratorServiceImpl;
 import com.lms.service.BorrowerServiceImpl;
 import com.lms.service.LibrarianServiceImpl;
@@ -31,10 +26,6 @@ public final class LMSMenu {
 	 * The helper for interacting with the user.
 	 */
 	private final MenuHelper mh;
-	/**
-	 * Our connection to the database.
-	 */
-	private final Connection connection;
 	/**
 	 * Logger for logging unexpected errors.
 	 */
@@ -53,10 +44,8 @@ public final class LMSMenu {
 	 * @param outStream    how to send output to the user
 	 * @param dbConnection how to connect to the database
 	 */
-	public LMSMenu(final Reader inStream, final Writer outStream,
-			final Connection dbConnection, final Clock clock) {
+	public LMSMenu(final Reader inStream, final Writer outStream, final Clock clock) {
 		mh = new MenuHelper(new Scanner(inStream), new PrintWriter(outStream));
-		connection = dbConnection;
 		this.clock = clock;
 	}
 
@@ -77,30 +66,13 @@ public final class LMSMenu {
 				case "0":
 					return;
 				case "1":
-					new BorrowerMenu(
-						new BorrowerServiceImpl(new LibraryBranchDaoImpl(connection),
-									new BookLoansDaoImpl(connection),
-									new CopiesDaoImpl(connection),
-									new BorrowerDaoImpl(connection), clock,
-									connection::commit, connection::rollback),
-							mh, clock).menu();
+					new BorrowerMenu(new BorrowerServiceImpl(), mh, clock).menu();
 					continue;
 				case "2":
-					new LibrarianMenu(
-							new LibrarianServiceImpl(new LibraryBranchDaoImpl(connection),
-									new BookDaoImpl(connection),
-									new CopiesDaoImpl(connection),
-									connection::commit, connection::rollback),
-							mh).menu();
+					new LibrarianMenu(new LibrarianServiceImpl(), mh).menu();
 					continue;
 				case "3":
-					new AdministratorMenu(new AdministratorServiceImpl(
-							new LibraryBranchDaoImpl(connection),
-							new BookDaoImpl(connection), new AuthorDaoImpl(connection),
-							new PublisherDaoImpl(connection),
-							new BookLoansDaoImpl(connection),
-							new BorrowerDaoImpl(connection), connection::commit,
-							connection::rollback), mh).menu();
+					new AdministratorMenu(new AdministratorServiceImpl(), mh).menu();
 					continue;
 				default:
 					mh.println("Please select role, or type 0 to quit");
@@ -111,6 +83,11 @@ public final class LMSMenu {
 			LOGGER.log(Level.SEVERE, "SQL error initializing DB connection", except);
 			mh.println(
 					"An unrecoverable error occurred while trying to connect to the database.");
+		} catch (final IOException except) {
+			LOGGER.log(Level.SEVERE,
+					"I/O error reading DB configuration properties file", except);
+			mh.println(
+					"An unrecoverable error occurred while reading database configuration parameters.");
 		}
 	}
 }
