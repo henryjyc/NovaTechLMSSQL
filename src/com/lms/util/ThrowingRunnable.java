ca5253e0847834d9006a9538d8d5fd04b4988254
Jonathan Lovelace <kingjon3377@gmail.com>
2019-04-08 11:19:15 -0400

    ThrowingRunnable: Improve Javadocs.

--- src/com/lms/util/ThrowingRunnable.java
+++ src/com/lms/util/ThrowingRunnable.java
@@ -4,9 +4,15 @@ package com.lms.util;
  * A no-arg method that may throw an exception. This is intended to be used as
  * the type for method references.
  *
+ * @param <E> the type of exception thrown
+ *
  * @author Jonathan Lovelace
  */
 @FunctionalInterface
 public interface ThrowingRunnable<E extends Throwable> {
+	/**
+	 * The method reference.
+	 * @throws E when thrown by the referenced method.
+	 */
 	void run() throws E;
 }
