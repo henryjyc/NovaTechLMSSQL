74e362a82c8880eaa7d35e1dd71dae643bb62f81
Jonathan Lovelace <kingjon3377@gmail.com>
2019-04-10 16:25:39 -0400

    AdministratorServiceTest: Fix wildcard import.

--- test/com/lms/service/AdministratorServiceTest.java
+++ test/com/lms/service/AdministratorServiceTest.java
@@ -1,6 +1,7 @@
 package com.lms.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
